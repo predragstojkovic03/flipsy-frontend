dbff2d4040e5dde896a38f1c79890b1c16921416
Fakultet Organizacionih Nauka <student2@prod.fon.bg.ac.rs>
2023-12-03 06:11:51 +0100

    finished subject finally

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
+import Subjects from './pages/Subjects/Subjects';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<HomePage />} />
+          <Route path='/predmeti' element={<Subjects />} />
         </Routes>
       </Router>
     </>
