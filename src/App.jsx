94b8b6a33e7a030166bdd9af979a54e999049e2c
Lakana22 <lazavaske22@gmail.com>
2023-12-03 03:42:35 +0100

    Finished question page markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
+import QuestionsPage from './pages/QuestionsPage/QuestionsPage';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<HomePage />} />
+          <Route path='/pitanja' element={<QuestionsPage />} />
         </Routes>
       </Router>
     </>
